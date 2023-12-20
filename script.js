93a0d0a1fb7ce2a3c2532168830a827da5bcc6b7
mdsaif123 <151512739+mdsaif123@users.noreply.github.com>
2023-12-20 21:05:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+
+let string = "";
+let buttons = document.querySelectorAll(".button");
+
+Array.from(buttons).forEach((saif) => {
+  saif.addEventListener('click', (e) => {
+    console.log(e.target);
+
+    if (e.target.innerHTML === "=") {
+      string = eval(string);
+      document.querySelector('input').value = string;
+    } else if (e.target.innerHTML === "AC" || e.target.innerHTML === "C") {
+      string = "  ";
+    } else {
+      string = string + e.target.innerHTML;
+      document.querySelector('input').value = string;
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let string = "";
+// let buttons = document.querySelectorAll(".button");
+
+// Array.from(buttons).forEach((m) => {
+//   m.addEventListener('click', (e) => {
+//     console.log(e.target); 
+
+//     if (e.target.innerHTML == '=') {
+//       string = eval(string);
+//       document.querySelector('input').value = string;
+
+
+//     } else if (e.target.innerHTML === 'C' || e.target.innerHTML === 'AC') {
+//       string = " ";
+
+//     }else {
+//       string = string + e.target.innerHTML;
+//       document.querySelector('input').value = string;
+//     }
+//   });
+// });
